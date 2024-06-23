33a0ba4d03e88aa6c904f54196a5f4b475336105
Jeeva <jeeva.rk@zohocorp.com>
2024-06-23 18:12:48 +0530

    Updated Views

---
+++ NotesApp/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  NotesApp
+//
+//  Created by jeeva-14880 on 23/06/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+        Text("Hello, World 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
